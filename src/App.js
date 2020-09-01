c751c64fddb7e7d45680ef911591aae483adc7dc
ACAPP-dev <acapp909@gmail.com>
2020-09-01 13:29:43 -0700

    added routing to app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './App.css';
 import NavBarContainer from './containers/NavBarContainer'
 import FooterContainer from './containers/FooterContainer'
+import Home from './components/Home'
+import About from './components/About'
+import Resume from './components/Resume'
 
 class App extends React.Component {
   render() {
-
-  
-    return (
+    return (  
       <div className="App">
         < NavBarContainer />
-        
-        <div className='main'>
-          <h1>Andrew's Portfolio Website</h1>
-        </div>
         < FooterContainer />
+        <Router>
+          <div className='main'>
+            <h1>Andrew's Portfolio Website</h1>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/resume' component={Resume} />
+          </div>
+        </Router>
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/Resume.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Resume = () => {
+    return (
+        <h2>Resume Component</h2>
+    )
+}
+
+export default Resume
