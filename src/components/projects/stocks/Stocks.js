1225e73f64bf395827125d91a30ce712b8b9da48
ACAPP-dev <acapp909@gmail.com>
2020-09-05 12:49:00 -0700

    get project readme markdown to render and update css

--- src/components/projects/stocks/Stocks.js
+++ src/components/projects/stocks/Stocks.js
@@ -1,18 +1,36 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
-import {readme} from './README.md'
+import readmeText from './README.md'
 
-const Stocks = () => {
+const input = '# This is a header\n\nAnd this is a paragraph'
 
-    return(
-        <div className='project-container'>
-            <h2>Stock Market Data Web App</h2>
-            <h4>August 2020</h4>
-            <div id='container'>
-                <ReactMarkdown source={readme} />
+class Stocks extends React.Component {
+
+    state = {
+        readme: ''
+    }
+
+    componentDidMount() {
+        fetch(readmeText)
+        .then(resp => resp.text())
+        .then(text => {
+            console.log(text)
+            this.setState({readme: text})
+        })
+
+    }
+
+    render() {
+        return(
+            <div className='project-container'>
+                <h2>Stock Market Data Web App</h2>
+                <h4>August 2020</h4>
+                
+                    <ReactMarkdown escapeHtml={false} source={this.state.readme} />
+                
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default Stocks
