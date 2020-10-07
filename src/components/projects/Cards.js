0acfaf36aba342bfc65e01f8dc0fea3e540027b2
ACAPP-dev <acapp909@gmail.com>
2020-10-07 14:26:43 -0700

    update cards to justify description text git push

--- src/components/projects/Cards.js
+++ src/components/projects/Cards.js
@@ -6,7 +6,7 @@ const Cards = props => {
     return(
         <div className='card-div'>
             <Link to={props.project.directory} exact='true'><h4>{props.project.name}</h4></Link>
-            <div className='card about-p'>{props.project.description}</div>
+            <div className='card-justify about-p'>{props.project.description}</div>
             <img className='card-img' src={require(`${props.project.picture}`)} alt='Project Screenshot' />
             <a className='card' href={props.project.video} target='_blank' rel="noopener noreferrer">Video Walkthrough</a>
             <a className='card' href={props.project.github} target='_blank' rel="noopener noreferrer">Github Link</a>
